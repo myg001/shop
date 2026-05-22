d0fabb18fd1de5a97384723b1b8663f39672f9dd
Bwaan <supriya4234@gmail.com>
2026-05-22 15:00:51 +0700

    server

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('<h1>สวัสดี! นี่คือเซิร์ฟเวอร์ พอร์ต 3000 ของคุณ</h1>');
-});
+app.use(express.static('public'));
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
